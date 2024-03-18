2749cb6d4234832991db83a7f23eff4941579268
douglas <douglas.arau@hotmail.com>
2024-03-18 15:11:33 -0300

    PessoaResource finalizado

--- src/main/java/br/com/fiap/springpfentregas/resource/PessoaResource.java
+++ src/main/java/br/com/fiap/springpfentregas/resource/PessoaResource.java
@@ -1,4 +1,32 @@
 package br.com.fiap.springpfentregas.resource;
 
+import br.com.fiap.springpfentregas.entity.Pessoa;
+import br.com.fiap.springpfentregas.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pessoa")
 public class PessoaResource {
+    @Autowired
+    private PessoaRepository repo;
+
+    @GetMapping
+    public List<Pessoa> findAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Pessoa findById(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Pessoa save(@RequestBody Pessoa pessoa ) {
+        return repo.save( pessoa );
+    }
 }
