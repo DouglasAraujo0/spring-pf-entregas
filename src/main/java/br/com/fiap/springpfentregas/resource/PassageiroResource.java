e6a83b32cac73adcd0636341d595c99fe7d4ff52
douglas <douglas.arau@hotmail.com>
2024-03-18 15:19:17 -0300

    passageiro e viagem resource finalizados.

--- src/main/java/br/com/fiap/springpfentregas/resource/PassageiroResource.java
+++ src/main/java/br/com/fiap/springpfentregas/resource/PassageiroResource.java
@@ -1,4 +1,35 @@
 package br.com.fiap.springpfentregas.resource;
 
+import br.com.fiap.springpfentregas.entity.Passageiro;
+import br.com.fiap.springpfentregas.entity.Pessoa;
+import br.com.fiap.springpfentregas.repository.PassageiroRepository;
+import br.com.fiap.springpfentregas.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/passageiro")
 public class PassageiroResource {
+
+    @Autowired
+    private PassageiroRepository repo;
+
+    @GetMapping
+    public List<Passageiro> findAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Passageiro findById(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Passageiro save(@RequestBody Passageiro passageiro ) {
+        return repo.save( passageiro );
+    }
 }
--- src/main/java/br/com/fiap/springpfentregas/resource/ViagemResource.java
+++ src/main/java/br/com/fiap/springpfentregas/resource/ViagemResource.java
@@ -1,4 +1,35 @@
 package br.com.fiap.springpfentregas.resource;
 
+import br.com.fiap.springpfentregas.entity.Pessoa;
+import br.com.fiap.springpfentregas.entity.Viagem;
+import br.com.fiap.springpfentregas.repository.PessoaRepository;
+import br.com.fiap.springpfentregas.repository.ViagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/viagem")
+
 public class ViagemResource {
+    @Autowired
+    private ViagemRepository repo;
+
+    @GetMapping
+    public List<Viagem> findAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Viagem findById(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Viagem save(@RequestBody Viagem viagem ) {
+        return repo.save( viagem );
+    }
 }
