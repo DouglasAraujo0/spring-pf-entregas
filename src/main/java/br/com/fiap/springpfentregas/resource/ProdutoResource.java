20be044a461d672604badf2f8aab775ec05868ed
douglas <douglas.arau@hotmail.com>
2024-03-18 15:13:45 -0300

    ProdutoResource finalizado

--- src/main/java/br/com/fiap/springpfentregas/resource/ProdutoResource.java
+++ src/main/java/br/com/fiap/springpfentregas/resource/ProdutoResource.java
@@ -1,4 +1,34 @@
 package br.com.fiap.springpfentregas.resource;
 
+
+import br.com.fiap.springpfentregas.entity.Produto;
+import br.com.fiap.springpfentregas.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoResource {
+
+    @Autowired
+    private ProdutoRepository repo;
+
+    @GetMapping
+    public List<Produto> findAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Produto findById(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Produto save(@RequestBody Produto produto) {
+        return repo.save( produto );
+    }
 }
